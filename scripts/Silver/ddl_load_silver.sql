fa4b896c65ba0552698c131ec49ff97d8b9ad5dd
omar abdullatif mohamed <168543342+omarabdullatiff@users.noreply.github.com>
2025-09-03 04:11:14 +0300

    Change date columns to use DATE type in crm_sales_details

--- scripts/Silver/ddl_load_silver.sql
+++ scripts/Silver/ddl_load_silver.sql
@@ -38,7 +38,7 @@ CREATE TABLE silver.crm_prd_info (
     dwh_create_data  DATETIME2 DEFAULT GETDATE()
 );
 GO
-
+  
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
 GO
@@ -47,9 +47,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
     sls_prd_key  NVARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
+    sls_order_dt date,
+    sls_ship_dt  date,
+    sls_due_dt   date,
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT,
