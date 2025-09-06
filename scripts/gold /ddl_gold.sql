052188f792a76bb954a7b29b711fec1d2d03718c
omar abdullatif mohamed <168543342+omarabdullatiff@users.noreply.github.com>
2025-09-07 01:27:52 +0300

    Create gold.fact_sales view for sales data
    Created a view for sales data including order details, customer, and product information.

--- scripts/gold /ddl_gold.sql
+++ scripts/gold /ddl_gold.sql
@@ -64,3 +64,32 @@ on p.cat_id = pc.id
 where p.prd_end_dt is null -- to remove historical data to simplifies queries and prevents double counting
 ;
 
+-- =============================================================================
+-- Create fact: gold.fact_sales
+-- =============================================================================
+
+if OBJECT_ID('gold.fact_sales' , 'V') is not null 
+    DROP VIEW gold.fact_sales ; 
+GO 
+
+CREATE VIEW gold.fact_sales
+as
+SELECT 
+    s.sls_ord_num as order_number,
+    p.product_key as procduct_key,
+    c.customer_key as customer_key,
+    s.sls_order_dt as order_date,
+    s.sls_ship_dt as ship_date,
+    s.sls_due_dt as due_date ,
+    s.sls_sales as sales_amount,
+    s.sls_quantity as quantity,
+    s.sls_price as price
+from silver.crm_sales_details s 
+LEFT JOIN  gold.dim_customers as c 
+on s.sls_cust_id = c.customer_id
+LEFT join gold.dim_products p
+on s.sls_prd_key = p.procduct_number
+;
+
+
+
